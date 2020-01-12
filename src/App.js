ace69a27834063e9ef9132191723d2d9d5d583be
Aris Vangelis <avangelis@byte.gr>
2020-01-12 16:57:33 +0200

    bigFeature explanation

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import { CardList } from './components/card-list/card-list.component';
 import { Search } from './components/search/search.component'
 import './App.css';
 
-//bigFeature branch
+//bigFeature branch explanation
 class App extends Component {
 
   constructor() {
